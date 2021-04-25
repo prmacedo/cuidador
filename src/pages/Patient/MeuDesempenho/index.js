bc1930c100a763700d12c5545702508c3a875e3a
prmacedo <p.ricardomacedo@hotmail.com>
2021-04-25 14:21:27 -0300

    responsive Meu Desempenho

--- src/pages/Patient/MeuDesempenho/index.js
+++ src/pages/Patient/MeuDesempenho/index.js
@@ -1,15 +1,34 @@
 import React from 'react';
+
 import BarChart from '../../../components/Charts/BarChart';
 import LineChart from '../../../components/Charts/LineChart';
 import MultipleLineChart from '../../../components/Charts/MultipleLineChart';
 
+import back from '../../../assets/images/icons/back.svg';
+import logoWhite from '../../../assets/images/logoAppWhite.svg';
+import logout from '../../../assets/images/icons/logout.svg';
+
 import './styles.css'
 
-export default function MeuDesempenho() {   
+export default function MeuDesempenho() {
+  window.addEventListener('scroll', () => {
+    if(window.scrollY >= 80) {
+      document.querySelector('#fixed-header').classList.add('scrolled');
+    } else {
+      document.querySelector('#fixed-header').classList.remove('scrolled');
+    }
+  })
+  
   return (  
     <>
-      <header></header>
-      <main>
+      <header id="fixed-header">
+        <div><span><img src={back} alt="Voltar" />Voltar</span></div>
+        <img src={logoWhite} alt="CuidaDor" className="logo"/>
+        <div><span><img src={logout} alt="Sair" />Sair</span></div>
+      </header>
+
+      <main id="meu-desempenho">
+        <h2>Meu Desempenho</h2>
         <BarChart />
         <hr/>
         <LineChart />

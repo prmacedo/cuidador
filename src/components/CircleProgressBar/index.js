5675598b6d2d1b886d132ee4e13d492f6f90bfde
prmacedo <p.ricardomacedo@hotmail.com>
2021-05-12 11:35:05 -0300

    create component CircleProgressBar

---
+++ src/components/CircleProgressBar/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function CircularProgressBar({ percentage, sqSize, strokeWidth }) {
+  const viewBox = `0 0 ${sqSize} ${sqSize}`;
+  const radius = (sqSize - strokeWidth) / 2;
+  const dashArray = radius * Math.PI * 2;
+  const dashOffset = dashArray - dashArray * percentage / 100;
+
+  return(
+    <svg
+      width={sqSize}
+      height={sqSize}
+      viewBox={viewBox}>
+      <circle
+        className="circle-background"
+        cx={sqSize / 2}
+        cy={sqSize / 2}
+        r={radius}
+        strokeWidth={`${strokeWidth}px`} />
+      <circle
+        className="circle-progress"
+        cx={sqSize / 2}
+        cy={sqSize / 2}
+        r={radius}
+        strokeWidth={`${strokeWidth}px`}
+        // Start progress marker at 12 O'Clock
+        transform={`rotate(-90 ${sqSize / 2} ${sqSize / 2})`}
+        style={{
+          strokeDasharray: dashArray,
+          strokeDashoffset: dashOffset
+        }} />
+      <text
+        className="circle-text"
+        x="50%"
+        y="50%"
+        dy=".3em"
+        textAnchor="middle">
+        {`${percentage}%`}
+      </text>
+    </svg>
+  );
+}

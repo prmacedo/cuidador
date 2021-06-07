ae3e8dd32b7a4f98f890cea47dd153560ea900c7
Matheus Algusto Oliveira dos Santos <you@example.com>
2021-06-07 01:00:43 -0300

    heroku cahnge

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 app,use('/', express.static(resolve(__dirname,'./build')))
 
 
-app.listen(process.env.PORT || 3000, (err) => {
-    if(err){ return console.log(err)}
+app.listen(process.env.PORT || 5000, (err) => {
+    if(err){ return console.log("Meu erro",err)}
     console.log('Tudo funcionando... :D')
 })
